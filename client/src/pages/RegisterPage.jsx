01518a8317d6d6651ad97c355f3389e3ca496dd0
edm1001 <edmer_franciz@yahoo.com>
2024-01-31 18:33:38 -0600

    init backend api

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import {useState} from "react";
 
 const RegisterPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('')
+  async function register(e) {
+    e.preventDefault();
+    await fetch('http://localhost:4000', {
+      method: 'POST',
+      body: JSON.stringify({username, password}),
+      headers: {'Content-Type' :'application/json'}
+    })
+  }
+
   return (
-    <form className="register">
+    <form className="register" onSubmit={register}>
       <h1>Register</h1>
-      <input type="text" placeholder="username" />
-      <input type="password" placeholder="password" />
+      <input type="text"
+       placeholder="username"
+       value={username}
+       onChange = { e => setUsername(e.target.value)}
+       />
+
+      <input type="password"
+       placeholder="password" 
+       value={password}
+        onChange={e => setPassword(e.target.value)}
+       />
       <button>Sign Up</button>
     </form>
   );
