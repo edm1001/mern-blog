8298a0928ea653471265a7e45ac600bb2f3bcec0
edm1001 <edmer_franciz@yahoo.com>
2024-02-07 17:09:59 -0600

    have password privacy on register

--- api/index.js
+++ api/index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const User = require('./models/User');
 const mongoose = require('mongoose');
-
 const app = express();
-const PORT = 4000
+const PORT = 4000;
+const bcrypt = require('bcrypt');
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json());
@@ -14,13 +15,14 @@ mongoose.connect('mongodb+srv://blog-user:blogpassword1@cluster0.gfbgpxl.mongodb
 app.post('/register', async (req,res) => {
     const {username, password} = req.body;
     try {
-      const userDoc = await User.create({username,password})
-      res.json({requestData: {username, password}});
+      const userDoc = await User.create({username,password:bcrypt.hashSync(password, salt),
+    })
+      res.json(userDoc)
     }catch(e) {
         res.status(400).json(e);
     }
 })
-
+// 1:07
 
 app.listen(PORT);
 
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import {useState} from 'react'
 
 const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  
   return (
       <form className='login'>
         <h1>Login</h1>
