797bd7304eccf042655feef241b546c05daf1f20
edm1001 <edmer_franciz@yahoo.com>
2024-02-09 17:14:36 -0600

    init create post

---
+++ client/src/pages/CreatePost.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+function CreatePost() {
+    const [title, setTitle] = useState('');
+    const [summary, setSummary] = useState('');
+    const [content, setContent] = useState('');
+    const modules = {
+        toolbar: [
+            [{ 'header': [1, 2, false] }],
+            ['bold', 'italic', 'underline','strike', 'blockquote'],
+            [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+            ['link', 'image'],
+            ['clean']
+        ]
+    };
+    const formats = [
+        'header',
+        'bold', 'italic', 'underline', 'strike', 'blockquote',
+        'list', 'bullet', 'indent',
+        'link', 'image'
+    ]
+
+  return (
+    <form>
+        <input type="title" placeholder={'Title'} value={title} />
+        <input type="summary" placeholder={'Summary'} />
+        <input type="file" />
+         <ReactQuill value={content} modules={modules} formats={formats} />
+         <button style={{marginTop:'8px'}}>Create Post!</button>
+    </form>
+  )
+}
+
+export default CreatePost
