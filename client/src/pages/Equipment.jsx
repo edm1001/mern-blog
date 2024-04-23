a29d76f31b38af05af5ca446e5906d39723c0dca
edm1001 <edmer_franciz@yahoo.com>
2024-04-23 17:37:06 -0500

    add to new pages

--- client/src/pages/Equipment.jsx
+++ client/src/pages/Equipment.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Header from "../components/Header";
 
 function Equipment() {
   return (
-    <div>Equipment</div>
-  )
+    <>
+      <Header />
+      <div className="text-center">
+        <h1 className="text-2xl">Equipment</h1>
+        <p className="mt-2">small paragraph about products we carry</p>
+      </div>
+      <div>
+        {/* all equipments unless a category is clicked */}
+        {/* cat. options = barbell, treadmill, yoga mats, books */}
+      </div>
+    </>
+  );
 }
 
-export default Equipment
+export default Equipment;
--- client/src/pages/Supplements.jsx
+++ client/src/pages/Supplements.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Header from '../components/Header';
 
 function Supplements() {
   return (
-    <div>Supplements Page</div>
+    <>
+    <Header />
+    <div className="text-center">
+      <h1 className="text-2xl">Supplements</h1>
+      <p className="mt-2">small paragraph about products we carry</p>
+    </div>
+    <div>
+      {/* all supplements unless a category is clicked */}
+      {/* cat. options = muscle building , energy&performance, Post-Workout & Recovery, Weight Management & FatLoss, Immune Support, Mental& Cognitive Boost, Endurance & Stamina, Overall Health & Wellness */}
+    </div>
+  </>
   )
 }
 
