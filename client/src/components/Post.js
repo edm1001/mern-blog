8e7fb5d02279ee07e790cc43c851069d6598b422
Edmer Valencia <edmerfranciz@gmail.com>
2024-12-04 12:59:47 -0600

    fix posts grid

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -4,41 +4,48 @@ import { Link } from "react-router-dom";
 
 const Post = ({ _id, title, summary, cover, createdAt, author, location }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4 hover:ring-8 cursor-pointer hover:scale-105 transition duration-200">
-      <Link to={`/post/${_id}`}>
-        <div className={`image ${!cover ? " h-48 bg-gray-200" : ""}`}>
-          {cover ? (
-            <img
-              src={"http://localhost:4000/" + cover}
-              alt={title}
-              className="rounded-lg w-full h-full object-cover"
-            />
-          ) : (
-            <div className="flex justify-center items-center h-full text-gray-500">
-              No Image
-            </div>
-          )}
-        </div>
-        <div className="texts flex flex-col justify-between p-4">
-          <h2 className="text-xl font-semibold mb-2 text-blue-400 hover:text-blue-700">
-            {title}
-          </h2>
-
-          <p className="info text-sm text-gray-600 hover:text-lightgray">
-            <p>{location}</p>
-            <p className="author text-gray-500 font-semibold hover:underline">
-              {author.username}
+    <div>
+      {/* Grid Container */}
+      <div className="gap-6 hover:scale-105 hover:ring-8">
+        {/* Individual Post */}
+        <Link
+          to={`/post/${_id}`}
+          className="cursor-pointer "
+        >
+          {/* Image Section */}
+          <div className={`image ${!cover ? "h-48 bg-gray-200" : ""}`}>
+            {cover ? (
+              <img
+                src={"http://localhost:4000/" + cover}
+                alt={title}
+                className="rounded-t-lg w-full h-48 object-cover"
+              />
+            ) : (
+              <div className="flex justify-center items-center h-full text-gray-500">
+                No Image
+              </div>
+            )}
+          </div>
+          {/* Text Section */}
+          <div className="flex flex-col justify-between p-4">
+            <h2 className="text-xl font-semibold mb-2 text-blue-400 hover:text-blue-700">
+              {title}
+            </h2>
+            <p className="info text-sm text-gray-600">
+              <span>{location}</span>
+              <span className="author text-gray-500 font-semibold hover:underline">
+                {author.username}
+              </span>
+              <time className="ml-2 text-gray-400">
+                {formatISO9075(new Date(createdAt))}
+              </time>
             </p>
-            <time className="ml-2 text-gray-400">
-              {formatISO9075(new Date(createdAt))}
-            </time>
-          </p>
-          <p className="summary mt-2 text-gray-500">{summary}</p>
-        </div>
-      </Link>
+            <p className="summary mt-2 text-gray-500">{summary}</p>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 };
-
 
 export default Post;
--- client/src/pages/BlogPage.jsx
+++ client/src/pages/BlogPage.jsx
@@ -1,5 +1,6 @@
 import Post from "../components/Post";
 import {useEffect, useState, useContext} from "react";
+import Hero from "../components/Hero";
 import {UserContext} from "../UserContext";
 
 // make this into blog Page, home will be somewhere else
@@ -27,10 +28,18 @@ export default function IndexPage() {
     });
   }, []);
   return (
-    <div className="my-12">
-      {posts.length > 0 && posts.map(post => (
-        <Post {...post} />
+<div className="container mx-auto">
+  <Hero/>
+  {/* Grid Container */}
+  <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 my-16 p-4">
+    {/* Map Over Posts */}
+    {posts.length > 0 &&
+      posts.map((post) => (
+        // Post Component Acts as a Grid Item
+        <Post key={post._id} {...post} />
       ))}
-    </div>
+  </div>
+</div>
+
   );
 }
