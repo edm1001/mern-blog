6094217017f89cd12dae147f52d0a05aadd54353
Edmer Valencia <edmerfranciz@gmail.com>
2024-12-03 23:59:23 -0600

    update ui to posts

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 // add location parameter
 const Post = ({ _id, title, summary, cover, createdAt, author, location }) => {
   return (
-    <div className="bg-white rounded-lg flex hover:shadow-md">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 hover:ring-8 cursor-pointer hover:scale-105 transition duration-200">
       <div className="image">
         <Link to={`/post/${_id}`}>
           <img
@@ -21,14 +21,16 @@ const Post = ({ _id, title, summary, cover, createdAt, author, location }) => {
             {title}
           </h2>
         </Link>
-        <p className="info text-sm text-gray-600">
+        <p className="info text-sm text-gray-600 hover:text-lightgray">
           <p>{location}</p>
-          <p className="author text-gray-800 font-semibold hover:underline">
+          <p className="author text-gray-500 font-semibold hover:underline">
             {author.username}
           </p>
-          <time className="ml-2">{formatISO9075(new Date(createdAt))}</time>
+          <time className="ml-2 text-gray-400">
+            {formatISO9075(new Date(createdAt))}
+          </time>
         </p>
-        <p className="summary mt-2 text-gray-700">{summary}</p>
+        <p className="summary mt-2 text-gray-500">{summary}</p>
       </div>
     </div>
   );
